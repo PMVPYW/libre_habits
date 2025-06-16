39307f1acc37269ab3def13f35da08c3a01173a7
Pedro Vicente <2242698@my.ipleiria.pt>
2025-06-16 21:39:40 +0100

    feat: update dependencies and configure Tailwind CSS with NativeWind
    - Added navigation dependencies for React Navigation.
    - Integrated NativeWind for styling with Tailwind CSS.
    - Configured TypeScript paths for easier imports.
    - Created Babel configuration for NativeWind support.
    - Added global CSS file for Tailwind directives.
    - Set up Metro bundler configuration for NativeWind.
    - Added TypeScript definitions for NativeWind.
    - Implemented a basic HabitsList component.
    - Configured Tailwind CSS with custom content paths.

--- LibreHabits/App.tsx
+++ LibreHabits/App.tsx
@@ -1,20 +1,65 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
+
+import HabitsList from '@/pages/Habits/HabitsList';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+
+const BottomTab = createBottomTabNavigator({
+  screens: {
+    Habits: HabitsList
+  }
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+          <StatusBar style="auto" />
+          <BottomTab.Navigator screenOptions={({ route }) => ({
+            tabBarStyle: {
+              borderRadius: 10,
+              marginHorizontal: 10,
+              elevation: 5,
+              shadowColor: 'rgb(255,125,72)',       // TODO --> Update with theme color
+              shadowOffset: { width: 0, height: -3 },
+              shadowOpacity: 0.1,
+              shadowRadius: 3,
+              height: 60,
+              bottom: 10,
+            },
+            headerShown: false,
+            animation: 'fade',
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName: string = '';
+
+              switch (route.name) {
+                case 'Habits':
+                  iconName = focused ? 'list' : 'list-outline';
+                  break;
+                default:
+                  iconName = focused ? 'alert-circle' : 'alert-circle-outline';
+                  break;
+              }
+
+
+              // Return the icon component
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+          >
+            <BottomTab.Screen name="Habits" component={HabitsList} />
+            <BottomTab.Screen name="Habits2" component={HabitsList} />
+          </BottomTab.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
