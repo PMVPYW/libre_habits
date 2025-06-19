b4cc36b6f28e15580efab0b8f12e0e9972423722
Pedro Vicente <2242698@my.ipleiria.pt>
2025-06-19 17:24:31 +0100

    chore: replace View with Pressable for HabitItem component to enhance interactivity

--- LibreHabits/pages/Habits/Components/HabitItem.tsx
+++ LibreHabits/pages/Habits/Components/HabitItem.tsx
@@ -1,5 +1,5 @@
 import { Habit } from "@/types";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 interface HabitItemProps {
     habit: Habit;
@@ -7,17 +7,19 @@ interface HabitItemProps {
 
 export default function HabitRenderItem({ habit }: HabitItemProps) {
     return (
-        <View className="w-full bg-slate-100 px-8 py-2">
-            <Text className="text-lg font-bold">{habit.title}</Text>
-            <View className="flex flex-row">
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('monday') ? "bg-slate-950 text-white" : "")}>M</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('tuesday') ? "bg-slate-950 text-white" : "")}>T</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('wednesday') ? "bg-slate-950 text-white" : "")}>W</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('thursday') ? "bg-slate-950 text-white" : "")}>T</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('friday') ? "bg-slate-950 text-white" : "")}>F</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('saturday') ? "bg-slate-950 text-white" : "")}>S</Text>
-                <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('sunday') ? "bg-slate-950 text-white" : "")}>S</Text>
+        <Pressable android_ripple={{ color: 'gray' /*customize with theme*/ }} className="w-full bg-slate-100 px-8 py-2">
+            <View>
+                <Text className="text-lg font-bold">{habit.title}</Text>
+                <View className="flex flex-row">
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('monday') ? "bg-slate-950 text-white" : "")}>M</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('tuesday') ? "bg-slate-950 text-white" : "")}>T</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('wednesday') ? "bg-slate-950 text-white" : "")}>W</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('thursday') ? "bg-slate-950 text-white" : "")}>T</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('friday') ? "bg-slate-950 text-white" : "")}>F</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('saturday') ? "bg-slate-950 text-white" : "")}>S</Text>
+                    <Text className={"w-8 h-8 border-2 border-slate-950 rounded-full mx-1 items-center justify-center flex text-center " + (habit.recurrency.includes('sunday') ? "bg-slate-950 text-white" : "")}>S</Text>
+                </View>
             </View>
-        </View>
+        </Pressable>
     )
 }
